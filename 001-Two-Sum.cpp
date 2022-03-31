1be9b2aeb2271c91dc360f057fb39b225b9a83a4
lijh <teenager_lijh@sina.com>
2022-03-31 12:59:01 +0800

    hello

---
+++ 001-Two-Sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+
+        
+        return res;
+    }
+};
+
+
+
+int main()
+{
+
+    cout<<"test123123"<<endl;
+    cout<<"test2222"<<endl;
+    cout<<"123123";
+
+    return 0;
+}
